8c39f392f572b2de8fd8757817f272c197f0bd56
Tri Denda <tridenda.nk@gmail.com>
2022-12-02 22:37:32 +0700

    re-implement take and drop functions

--- One.hs
+++ One.hs
@@ -1,5 +1,40 @@
 module One where
 
-square x = x * x
+-- Data -> Function -> Data
 
-cube x = x*x*x
+addDua x = x + 2
+
+myName "Aziz" = "Nama saya Aziz"
+myName "Denda" = "Nama saya Denda"
+
+isGreaterThanTwo x
+  | x > 2 = "Lebih cooyyy"
+  | x < 2 = "alah kurang"
+
+myName2 name
+  | name == "Aziz" = "Aziz nihhh"
+  | name == "Denda" = "Dendaaa"
+  | otherwise = "Saha etaa"
+
+isGreaterThanLego x
+  | x > lego = "Lebiiihhh"
+  | x <= lego = "nyaa kurang"
+  where lego = 5
+
+-- FUnction -> Function
+
+-- take' 2 [1,2,3,4,5]
+
+take' m [] = []
+take' 0 _ = []
+take' m (x:xs) = x : take' (m-1) xs
+
+-- take' 2 [1,2,3,4,5] => 1 : take' (2-1) [2,3,4,5]
+-- take' 1 [2,3,4,5] = 2 : take' (1-1) [3,4,5]
+-- take' 0 [3,4,5] = []
+
+-- take' 2 [] => x 
+
+myDrop m [] = []
+myDrop 0 xs = xs
+myDrop m (x:xs) = myDrop (m-1) xs
