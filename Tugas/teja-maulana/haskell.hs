ebaaf11081901c262d95b8dc2c4c2d7d2753494b
Tejamaul <141901432+Tejamaul@users.noreply.github.com>
2023-08-11 13:40:44 -0600

    Belajar tipe data dan Function

--- Tugas/teja-maulana/haskell.hs
+++ Tugas/teja-maulana/haskell.hs
@@ -1,4 +1,19 @@
 module Haskell where
 
 myName = "Teja Maulana"
-myData = 1
+-- Primitif
+myData = 1234567
+myChar = 'a'
+myBooleans = [True, False, (True && False), (False || True)]
+-- Collactions
+myList = [1..100]
+myTuple = ()
+myString = ""
+
+tambahLima x "a" = x + 5
+tambahLima x "b" = x + 6
+
+apakahAziz nama
+    | nama == "Aziz" = True
+    | nama == "Denda" = False
+    
