366cfdb1c71f56f8537da835c90ad5c374f26dc5
siliwangidigitalsolutions <146359763+siliwangidigitalsolutions@users.noreply.github.com>
2023-10-08 02:23:45 +0700

    ngajarin sampe where

--- Tugas/siliwa/contoh.hs
+++ Tugas/siliwa/contoh.hs
@@ -1,2 +1,33 @@
 module Contoh where
 
+square x = x * x
+
+-- nama "Aziz" = "Keluarin Aziz"
+-- nama "Denda" = "Ini Denda"
+
+-- cariOrg nama = if (nama == "Aziz") then "Keluarin Aziz" else "Ini Denda"
+
+cariOrg nama
+  | nama == "Aziz" = "Keluarin Aziz"
+  | nama == "Denda" = "Ini Denda"
+  | nama == "Teja" = "Ini tejaaaa"
+  | otherwise = "Dimassssss"
+
+-- cariGede x
+--   | x > 5 = "GEdeeeeeeeeeeeeeee"
+--   | otherwise = "kecil"
+
+myNumber = 5
+
+cariGede x
+  | x > myNumber = "Gedeeeeee"
+  | otherwise = "Kecil"
+
+cariGedeBanget x
+  | x > myLocalNumber = "Gede bangeett"
+  | otherwise = "Kecilllll"
+  where myLocalNumber = 99
+
+cariGedeAjah x
+  | x > myLocalNumber = "gedeee"
+  | otherwise = "kecil"
