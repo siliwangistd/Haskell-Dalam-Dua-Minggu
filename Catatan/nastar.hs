fbdddb8504fb12fa632a3a4e02b8aa095e22455f
siliwangidigitalsolutions <146359763+siliwangidigitalsolutions@users.noreply.github.com>
2025-03-31 00:01:28 +0700

    ajar ulang nanda

---
+++ Catatan/nastar.hs
@@ -0,0 +1,25 @@
+-- source code
+module Nastar where
+
+hello = "Hello, World!"
+
+square x y = x * x
+
+-- programming isn't about typing, it's about thinking
+
+-- nama "Denda" = "Ini Denda"
+-- nama "Nanda" = "Ini Nanda"
+
+-- cariNama namaSaya
+  -- | namaSaya == "Denda" = "Ini Denda"
+  -- | namaSaya == "Nanda" = "Ini Nanda"
+  -- | otherwise = "Ini adit lohh"
+
+cariNama x y = x ++ "Bisa cooyyy"
+
+
+-- tau inputnya apa? outputnya apa? <<-- PENTINGG!!!
+
+
+
+
